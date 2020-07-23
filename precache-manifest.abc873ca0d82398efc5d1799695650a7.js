3122e06eab7e3855e303cf1fc72867405c574af8
rootrsk <ravishankar7050@gmail.com>
2020-07-23 23:26:54 +0530

    Updates

---
+++ precache-manifest.abc873ca0d82398efc5d1799695650a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fceb7e422f118ae56a344e52c0498448",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c97e282e7a8b58670753",
+    "url": "/static/css/main.0e8a8718.chunk.css"
+  },
+  {
+    "revision": "20d05f3558e9779e1dcb",
+    "url": "/static/js/2.de2b59a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.de2b59a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c97e282e7a8b58670753",
+    "url": "/static/js/main.d2a77ed1.chunk.js"
+  },
+  {
+    "revision": "7dea87a2a80e913c35cf",
+    "url": "/static/js/runtime-main.9b271064.js"
+  }
+]);
